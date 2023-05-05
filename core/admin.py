33867e8545f560f53e2c91ef802e9f0d063ef6c0
Isadora Moresco <isamoresco@gmail.com>
2023-05-05 01:37:46 -0300

    adicionanado as models no admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Cadastro_Crianca, Cadastro_Evolucao_Crianca, Cadastro_Funcionario, Unidade_Atentimento, Cadastro_Vacina_Aplicada, Cadastro_Consultas #new
 
-# Register your models here.
+admin.site.register(Cadastro_Crianca)
+admin.site.register(Cadastro_Evolucao_Crianca)
+admin.site.register(Cadastro_Funcionario) 
+admin.site.register(Unidade_Atentimento) 
+admin.site.register(Cadastro_Vacina_Aplicada) 
+admin.site.register(Cadastro_Consultas) 
