ed9d05acee669597c08c63aa2bd1b477c35c7b4a
Isadora Moresco <isamoresco@gmail.com>
2023-05-05 16:29:41 -0300

    criação url cadastro_usuario e alteração na url login

--- caderneta_crianca/urls.py
+++ caderneta_crianca/urls.py
@@ -1,26 +1,13 @@
-"""caderneta_crianca URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('login', views.login, name='login'),
+    path('cadastro', views.cadastro_usuario, name="cadastro"),
+    path('login', views.login, name="login"),
+    # path('login', views.login, name='login'),
     path('esqueci_senha', views.esqueci_senha, name='esqueci_senha'),
 
     path('', views.home, name='home'),
