0bc9c9df4285bcfd5c7d0a40a13a2ebde7d99df5
Isadora Moresco <isadora.moresco@ambevtech.com.br>
2023-05-17 22:41:02 -0300

    mudança na models + banco de dados com as migrações

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from .models import Cadastro_Crianca, Cadastro_Evolucao_Crianca, Cadastro_Funcionario, Unidade_Atentimento, Cadastro_Vacina_Aplicada, Cadastro_Consultas #new
+from .models import Cadastro_Crianca, Cadastro_Evolucao_Crianca, Cadastro_Funcionario, Unidade_Atentimento, Cadastro_Vacina_Aplicada, Cadastro_Consultas_Medicas, Cadastro_Consultas_Odontologicas #new
 
 admin.site.register(Cadastro_Crianca)
 admin.site.register(Cadastro_Evolucao_Crianca)
 admin.site.register(Cadastro_Funcionario) 
 admin.site.register(Unidade_Atentimento) 
 admin.site.register(Cadastro_Vacina_Aplicada) 
-admin.site.register(Cadastro_Consultas) 
+admin.site.register(Cadastro_Consultas_Medicas)
+admin.site.register(Cadastro_Consultas_Odontologicas)
+
+                     
