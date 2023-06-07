33335ad1dcc428e4166ef4b1f6c8616b1c1d0366
Isadora Moresco <isamoresco@gmail.com>
2023-06-07 00:36:24 -0300

    adição de nova tabela no admin

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Cadastro_Crianca, Cadastro_Evolucao_Crianca, Cadastro_Funcionario, Unidade_Atentimento, Cadastro_Vacina_Aplicada, Cadastro_Consultas_Medicas, Cadastro_Consultas_Odontologicas #new
+from .models import Cadastro_Crianca, Cadastro_Evolucao_Crianca, Cadastro_Funcionario, Unidade_Atentimento, Cadastro_Vacina_Aplicada, Cadastro_Consultas_Medicas, Cadastro_Consultas_Odontologicas, Observacoes #new
 
 admin.site.register(Cadastro_Crianca)
 admin.site.register(Cadastro_Evolucao_Crianca)
@@ -8,5 +8,6 @@ admin.site.register(Unidade_Atentimento)
 admin.site.register(Cadastro_Vacina_Aplicada) 
 admin.site.register(Cadastro_Consultas_Medicas)
 admin.site.register(Cadastro_Consultas_Odontologicas)
+admin.site.register(Observacoes)
 
                      
